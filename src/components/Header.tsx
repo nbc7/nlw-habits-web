e8f796c2d184e31064ad1564316ff5b3f596f7c3
nbc7 <nathanborgescastro@gmail.com>
2023-05-03 19:21:07 -0300

    fixed button "Novo hábito" showing up when user is not logged in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -95,31 +95,37 @@ export function Header() {
       </Link>
 
       <div className="flex gap-3">
-        <Dialog.Root>
-          <Dialog.Trigger
-            type="button"
-            className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background"
-          >
-            <Plus size={20} className="text-violet-500" />
-            Novo hábito
-          </Dialog.Trigger>
+        {!isAuthenticated ? (
+          <Login isDisabled={isUserLoading} />
+        ) : (
+          <>
+            <Dialog.Root>
+              <Dialog.Trigger
+                type="button"
+                className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background"
+              >
+                <Plus size={20} className="text-violet-500" />
+                Novo hábito
+              </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+              <Dialog.Portal>
+                <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
 
-            <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-              <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-lg hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900">
-                <X size={24} aria-label="Fechar" />
-              </Dialog.Close>
+                <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                  <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-lg hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900">
+                    <X size={24} aria-label="Fechar" />
+                  </Dialog.Close>
 
-              <Dialog.Title className="text-3xl leading-tight font-extrabold">Criar hábito</Dialog.Title>
+                  <Dialog.Title className="text-3xl leading-tight font-extrabold">Criar hábito</Dialog.Title>
 
-              <NewHabitForm />
-            </Dialog.Content>
-          </Dialog.Portal>
-        </Dialog.Root>
+                  <NewHabitForm />
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
 
-        {!isAuthenticated ? <Login isDisabled={isUserLoading} /> : <Logout />}
+            <Logout />
+          </>
+        )}
       </div>
     </div>
   );
