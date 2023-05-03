f49b5e9f4f494e004a103f39b567a3feb7082fef
nbc7 <nathanborgescastro@gmail.com>
2023-05-03 19:16:11 -0300

    added style

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -2,15 +2,26 @@ import '../styles/global.css';
 import '../lib/dayjs';
 
 import { Header } from '../components/Header';
-import { SummaryTable } from '../components/SummaryTable';
 
 export function Error404() {
   return (
-    <div className="w-screen h-screen flex justify-center mt-56">
+    <div className="w-screen h-screen flex justify-center pt-56">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
 
-        <h1>404 - Not found</h1>
+        <div className="flex flex-col items-center">
+          <div className="flex justify-center items-center">
+            <p className="text-violet-500 text-[256px] font-bold">404</p>
+            <p className="absolute font-semibold text-2xl drop-shadow">A página que você está procurando não existe</p>
+          </div>
+
+          <a
+            href="/"
+            className="border font-semibold rounded-lg px-6 py-4 flex items-center gap-3 transition-colors border-violet-500 bg-violet-500 hover:border-violet-300 hover:bg-violet-300 focus:ring-violet-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-background"
+          >
+            Página inicial
+          </a>
+        </div>
       </div>
     </div>
   );
