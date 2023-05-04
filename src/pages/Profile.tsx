f77c036dba4bb3d5e1f018c5156e0d91adbd3715
nbc7 <nathanborgescastro@gmail.com>
2023-05-04 14:35:04 -0300

    added user profile (avatar + name)

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,13 +3,24 @@ import '../lib/dayjs';
 
 import { Header } from '../components/Header';
 import { SummaryTable } from '../components/SummaryTable';
+import { useAuth } from '../hooks/useAuth';
 
 export function Profile() {
+  const { user } = useAuth();
+
   return (
-    <div className="w-screen h-screen flex justify-center mt-56">
+    <div className="w-screen h-screen flex justify-center pt-56">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
-        Profile
+
+        {user && (
+          <div className="flex items-center justify-center gap-6">
+            <img src={user.avatarUrl} alt="foto de perfil" className="h-16 w-16 rounded-full" />
+
+            <p className="font-semibold text-xl">{`${user.name.substring(0, 60)}${user.name.length > 60 ? '...' : ''}`}</p>
+          </div>
+        )}
+
         <SummaryTable />
       </div>
     </div>
